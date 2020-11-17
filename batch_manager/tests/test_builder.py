c587c94aa5f138397fc89b8f28b2b8d410d0852e
Andrey Chertkov <andreychertckov@gmail.com>
2020-11-17 09:14:55 +0000

    Make models global in tests

--- batch_manager/tests/test_builder.py
+++ batch_manager/tests/test_builder.py
@@ -13,6 +13,29 @@ from src.builder import (
 )
 
 
+stub_model = dm.ModelObject(
+    "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=128
+)
+small_batch_stub_model = dm.ModelObject(
+    "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=2
+)
+blur_model = dm.ModelObject(
+    "blur", "registry.visionhub.ru/models/blur:v3", stateless=True, batch_size=128
+)
+stub_stateful_model = dm.ModelObject(
+    "stub",
+    "registry.visionhub.ru/models/stub-stateful:v3",
+    stateless=False,
+    batch_size=128,
+)
+blur_stateful_model = dm.ModelObject(
+    "blur",
+    "registry.visionhub.ru/models/blur-stateful:v3",
+    stateless=False,
+    batch_size=128,
+)
+
+
 def test_batch_build_empty():
     """Test for zero case"""
     assert build_batches([]) == dm.Batches(batches=[])
@@ -27,18 +50,19 @@ def string_generator():
 
 def test_batch_build_one():
     """Test build for one request object"""
-    model = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=128
-    )
     request_object = dm.RequestObject(
-        "test", np.array([1, 2, 3, 4]), "internal_123123", parameters={}, model=model
+        "test",
+        np.array([1, 2, 3, 4]),
+        "internal_123123",
+        parameters={},
+        model=stub_model,
     )
 
     expected_batch = dm.BatchObject(
         next(string_generator()),
         [np.array([1, 2, 3, 4])],
         [{}],
-        model=model,
+        model=stub_model,
         request_objects=[request_object],
     )
 
@@ -50,33 +74,27 @@ def test_batch_stateless_many():
     """
     Test build for many stateless request objects
     """
-    model1 = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=128
-    )
-    model2 = dm.ModelObject(
-        "blur", "registry.visionhub.ru/models/blur:v3", stateless=True, batch_size=128
-    )
     uid_generator = string_generator()
     request_object1 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={"gif_id": 12},
-        model=model1,
+        model=stub_model,
     )
     request_object2 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model2,
+        model=blur_model,
     )
     request_object3 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model1,
+        model=stub_model,
     )
 
     batch_uid_generator = string_generator()
@@ -87,14 +105,14 @@ def test_batch_stateless_many():
                 uid=next(batch_uid_generator),
                 inputs=[request_object1.inputs, request_object3.inputs],
                 parameters=[request_object1.parameters, request_object3.parameters],
-                model=model1,
+                model=stub_model,
                 request_objects=[request_object1, request_object3],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object2.inputs],
                 parameters=[request_object2.parameters],
-                model=model2,
+                model=blur_model,
                 request_objects=[request_object2],
             ),
         ]
@@ -110,33 +128,27 @@ def test_batch_stateful_many():
     """
     Test build for many stateful request objects
     """
-    model1 = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=False, batch_size=128
-    )
-    model2 = dm.ModelObject(
-        "blur", "registry.visionhub.ru/models/blur:v3", stateless=False, batch_size=128
-    )
     uid_generator = string_generator()
     request_object1 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={"gif_id": 12},
-        model=model1,
+        model=stub_stateful_model,
     )
     request_object2 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_124",
         parameters={},
-        model=model2,
+        model=blur_stateful_model,
     )
     request_object3 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={"gif_id": 12},
-        model=model1,
+        model=stub_stateful_model,
     )
 
     batch_uid_generator = string_generator()
@@ -147,14 +159,14 @@ def test_batch_stateful_many():
                 uid=next(batch_uid_generator),
                 inputs=[request_object1.inputs, request_object3.inputs],
                 parameters=[request_object1.parameters, request_object3.parameters],
-                model=model1,
+                model=stub_stateful_model,
                 request_objects=[request_object1, request_object3],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object2.inputs],
                 parameters=[request_object2.parameters],
-                model=model2,
+                model=blur_stateful_model,
                 request_objects=[request_object2],
             ),
         ]
@@ -170,30 +182,6 @@ def test_multi_stage_build_batch():
     """
     Test build batch if some batches are provided
     """
-    model_stateless1 = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=128
-    )
-
-    model_stateless2 = dm.ModelObject(
-        "stub-public",
-        "registry.visionhub.ru/models/stub:v3",
-        stateless=True,
-        batch_size=128,
-    )
-
-    model_stateful1 = dm.ModelObject(
-        "stub-stateful",
-        "registry.visionhub.ru/models/stub:v3",
-        stateless=False,
-        batch_size=128,
-    )
-
-    model_stateful2 = dm.ModelObject(
-        "stub-public-stateful",
-        "registry.visionhub.ru/models/stub:v3",
-        stateless=False,
-        batch_size=128,
-    )
 
     uid_generator = string_generator()
     request_object1 = dm.RequestObject(
@@ -201,49 +189,49 @@ def test_multi_stage_build_batch():
         inputs=np.array(range(10)),
         source_id="internal_123_124",
         parameters={"gif_id": 12},
-        model=model_stateless1,
+        model=stub_model,
     )
     request_object2 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model_stateful2,
+        model=blur_stateful_model,
     )
     request_object3 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_125",
         parameters={},
-        model=model_stateful1,
+        model=stub_stateful_model,
     )
     request_object4 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_121",
         parameters={},
-        model=model_stateful1,
+        model=stub_stateful_model,
     )
     request_object5 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model_stateful2,
+        model=blur_stateful_model,
     )
     request_object6 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model_stateful2,
+        model=blur_stateful_model,
     )
     request_object7 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_127",
         parameters={"gif_id": 12},
-        model=model_stateless2,
+        model=blur_model,
     )
 
     batch_uid_generator = string_generator()
@@ -253,7 +241,7 @@ def test_multi_stage_build_batch():
                 uid=next(batch_uid_generator),
                 inputs=[request_object1.inputs],
                 parameters=[request_object1.parameters],
-                model=model_stateless1,
+                model=stub_model,
                 request_objects=[request_object1],
             ),
             dm.BatchObject(
@@ -268,28 +256,28 @@ def test_multi_stage_build_batch():
                     request_object5.parameters,
                     request_object6.parameters,
                 ],
-                model=model_stateful2,
+                model=blur_stateful_model,
                 request_objects=[request_object2, request_object5, request_object6],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object3.inputs],
                 parameters=[request_object3.parameters],
-                model=model_stateful1,
+                model=stub_stateful_model,
                 request_objects=[request_object3],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object4.inputs],
                 parameters=[request_object4.parameters],
-                model=model_stateful1,
+                model=stub_stateful_model,
                 request_objects=[request_object4],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object7.inputs],
                 parameters=[request_object7.parameters],
-                model=model_stateless2,
+                model=blur_model,
                 request_objects=[request_object7],
             ),
         ]
@@ -312,30 +300,27 @@ def test_batch_size_limit():
     """
     Test batch size limitation
     """
-    model1 = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=2
-    )
     uid_generator = string_generator()
     request_object1 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={"gif_id": 12},
-        model=model1,
+        model=small_batch_stub_model,
     )
     request_object2 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model1,
+        model=small_batch_stub_model,
     )
     request_object3 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={},
-        model=model1,
+        model=small_batch_stub_model,
     )
 
     batch_uid_generator = string_generator()
@@ -346,14 +331,14 @@ def test_batch_size_limit():
                 uid=next(batch_uid_generator),
                 inputs=[request_object1.inputs, request_object2.inputs],
                 parameters=[request_object1.parameters, request_object2.parameters],
-                model=model1,
+                model=small_batch_stub_model,
                 request_objects=[request_object1, request_object2],
             ),
             dm.BatchObject(
                 uid=next(batch_uid_generator),
                 inputs=[request_object3.inputs],
                 parameters=[request_object3.parameters],
-                model=model1,
+                model=small_batch_stub_model,
                 request_objects=[request_object3],
             ),
         ]
@@ -369,16 +354,13 @@ def test_build_one_mapping_batch():
     """
     Test for build mapping batch for one case
     """
-    model1 = dm.ModelObject(
-        "stub", "registry.visionhub.ru/models/stub:v3", stateless=True, batch_size=2
-    )
     uid_generator = string_generator()
     request_object1 = dm.RequestObject(
         uid=next(uid_generator),
         inputs=np.array(range(10)),
         source_id="internal_123_123",
         parameters={"gif_id": 12},
-        model=model1,
+        model=stub_model,
     )
     batches = build_batches([request_object1], uid_generator=string_generator())
 
@@ -395,24 +377,18 @@ def test_split_batches():
     """
     Test for split_on_complete_and_uncomplete_batches
     """
-    model = dm.ModelObject(
-        name="stub",
-        address="registry.visionhub.ru/models/stub:v3",
-        stateless=True,
-        batch_size=2,
-    )
     batch1 = dm.BatchObject(
         uid="00",
         inputs=[np.array(range(10)), np.array(range(10))],
         parameters=[{}, {}],
-        model=model,
+        model=small_batch_stub_model,
         request_objects=[],
     )
     batch2 = dm.BatchObject(
         uid="01",
         inputs=[np.array(range(10))],
         parameters=[{}],
-        model=model,
+        model=small_batch_stub_model,
         request_objects=[],
     )
 

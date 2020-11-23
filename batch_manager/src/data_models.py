2894d7514e4213bd9a8f74e46e3a7859c67471b3
Andrey Chertkov <andreychertckov@gmail.com>
2020-11-23 13:40:25 +0000

    Make shared modules

--- batch_manager/src/data_models.py
+++ batch_manager/src/data_models.py
@@ -4,11 +4,15 @@ Data object definitions
 __author__ = "Andrey Chertkov"
 __email__ = "a.chertkov@eora.ru"
 
-import json
 from dataclasses import dataclass, field
-from typing import List, Tuple
+from typing import List
 
-import numpy as np  # type: ignore
+from shared_modules.data_objects import (
+    ModelObject,
+    RequestObject,
+    MinimalBatchObject,
+    BatchMapping,
+)
 
 
 @dataclass
@@ -35,104 +39,6 @@ class Config:
     db_file: str
     create_db_file: bool
     send_batch_timeout: float
-
-
-@dataclass
-class ModelObject:
-    """
-    Represents model. Need for Task manager to know which model and how to start it.
-
-    Parameters
-    ----------
-    name:
-        Name of the model
-    address:
-        Docker image address
-    stateless:
-        Flag, if true then model stateless, else statefull
-    batch_size:
-        Default batch size
-    """
-
-    name: str
-    address: str
-    stateless: bool
-    batch_size: int
-
-
-@dataclass(eq=False)
-class RequestObject:
-    """
-    Format of input data.
-
-    Parameters
-    ----------
-    uid:
-        Uniq identifier of the request
-    inputs:
-        Input tensor, which will be processed
-    source_id:
-        Combination of adapter_id and user_id, need for result routing
-    parameters:
-        Meta information for processing
-    model:
-        Information about model
-    """
-
-    uid: str
-    inputs: np.ndarray
-    source_id: str
-    parameters: dict
-    model: ModelObject
-
-    def __eq__(self, other):
-        if not isinstance(other, type(self)):
-            return False
-        return (
-            self.uid == other.uid
-            and np.array_equal(self.inputs, other.inputs)
-            and self.parameters == other.parameters
-            and self.model == other.model
-        )
-
-
-@dataclass(eq=False)
-class MinimalBatchObject:
-    """
-    Format of BatchManager output data.
-    This object will be sent over the zmq output socket
-
-    Parameters
-    ----------
-    uid:
-        Uniq identifier of the batch
-    inputs:
-        List of tensors, that will be processed.
-    parameters:
-        List of meta information for processing. Order is equal to order of inputs
-    model:
-        Information about model
-    """
-
-    uid: str
-    inputs: List[np.ndarray]
-    parameters: List[dict]
-    model: ModelObject
-
-    @property
-    def size(self) -> int:
-        "Actual batch size"
-        return len(self.inputs)
-
-    def __eq__(self, other):
-        if not isinstance(other, type(self)):
-            return False
-        return (
-            self.uid == other.uid
-            and map(np.array_equal, zip(self.inputs, other.inputs))
-            and self.parameters == other.parameters
-            and self.model == other.model
-        )
 
 
 @dataclass(eq=False)
@@ -176,38 +82,6 @@ class BatchObject(MinimalBatchObject):
             and self.request_objects == other.request_objects
             and self.source_id == other.source_id
         )
-
-
-@dataclass
-class BatchMapping:
-    """
-    Mapping between RequestObjects and BatchObject
-
-    Parameters
-    ----------
-    batch_uid:
-        Uniq identifier of batch
-    request_object_uids:
-        Uniq identifiers of RequestObjects
-    source_ids:
-        Ids of sourec
-    """
-
-    batch_uid: str
-    request_object_uids: List[str]
-    source_ids: List[str]
-
-    def to_key_value(self) -> Tuple[bytes, bytes]:
-        """
-        Make key value tuple, that will be stored in LevelDB
-        """
-        key = self.batch_uid.encode("utf-8")
-        value = json.dumps(
-            dict(
-                request_object_uids=self.request_object_uids, source_ids=self.source_ids
-            )
-        ).encode("utf-8")
-        return key, value
 
 
 @dataclass

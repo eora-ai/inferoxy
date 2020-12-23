1d899f2b3888a515d6b8e294970b4e05891227df
gafmn <madina.gafarova@yandex.ru>
2020-12-23 14:59:26 +0300

    Implement stop instance

--- task_manager/tests/test_cloud_client.py
+++ task_manager/tests/test_cloud_client.py
@@ -4,6 +4,7 @@ Test cloud client
 import sys
 import yaml
 import pytest
+import time
 
 sys.path.append("..")
 
@@ -11,16 +12,20 @@ from src.cloud_client import DockerCloudClient
 
 import src.data_models as dm
 
+
 model_object_fail = dm.ModelObject(
     name="fail", address="akdfjka", stateless=True, batch_size=24
 )
-model_object_fail = dm.ModelObject(
+model_object_pass = dm.ModelObject(
     name="run", address="nginx", stateless=True, batch_size=24
 )
 
 
 def test_image_doesnt_exist():
-
+    """
+    Test function start_instance()
+    Throw exceprion RuntimeError -> image does not exist
+    """
     with open("../config.yaml") as config_file:
         config_dict = yaml.full_load(config_file)
         config = dm.Config(**config_dict)
@@ -29,3 +34,28 @@ def test_image_doesnt_exist():
     with pytest.raises(RuntimeError) as exc:
         docker_client.start_instance(model_object_fail)
     assert "Image not found" in str(exc.value)
+
+
+def run():
+    with open("../config.yaml") as config_file:
+        config_dict = yaml.full_load(config_file)
+        config = dm.Config(**config_dict)
+
+    docker_client = DockerCloudClient(config)
+    docker_client.start_instance(model_object_pass)
+
+
+def test_stop_container():
+    with open("../config.yaml") as config_file:
+        config_dict = yaml.full_load(config_file)
+        config = dm.Config(**config_dict)
+
+    docker_client = DockerCloudClient(config)
+    model_instance = docker_client.start_instance(model_object_pass)
+    time.sleep(10)
+    docker_client.stop_instance(model_instance)
+    # TODO: think about how to check that container is stoped
+
+
+if __name__ == "__main__":
+    test_stop_container()

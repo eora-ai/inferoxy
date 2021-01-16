48a15e652e09c1bba5dd621047532104d2945958
gafmn <madina.gafarova@yandex.ru>
2021-01-16 12:48:50 +0300

    Implementation Sender and Receiver

--- task_manager/src/utils/data_transfers/receiver.py
+++ task_manager/src/utils/data_transfers/receiver.py
@@ -6,28 +6,64 @@ __author__ = "Andrey Chertkov"
 __name__ = "a.chertkov@eora.ru"
 
 import zmq
-from typing import Dict
+import sys
 
-import src.data_models as dm
+from loguru import logger
+
+from shared_modules.data_objects import ResponseBatch
 
 
 class Receiver:
     def __init__(self, open_address, sync_address, settings):
         self.zmq_context = zmq.Context()
         self.zmq_socket = self.zmq_context.socket(zmq.PULL)
-        self.zmq_socket.setsockopt(zmq.RCVHWM, settings.get("RCVHWM", 10))
-        self.zmq_socket.setsockopt(zmq.RCVTIMEO, settings.get("RCVTIMEO", 60000))
-        self.zmq_socket.connect(open_address)
-        # TODO Add sync
-
-    async def sync(self):
-        pass
-
-    async def receive(self, data: Dict) -> dm.ResponseBatch:
-        # TODO: Field: inputs, parameters, model, status - ???
-        await dm.ResponseBatch(
-            uid=data.get("uid"),
+        self.zmq_socket.setsockopt(zmq.RCVHWM, settings.ZMQ_SETTINGS.get("RCVHWM", 10))
+        self.zmq_socket.setsockopt(
+            zmq.RCVTIMEO, settings.ZMQ_SETTINGS.get("RCVTIMEO", 60000)
         )
+        self.zmq_socket.connect(open_address)
+        self.sync(sync_address, settings)
+
+    def sync(self, sync_address, settings):
+        ctx = zmq.Context.instance()
+        r = ctx.socket(zmq.REQ)
+        r.setsockopt(zmq.SNDTIMEO, settings.ZMQ_SETTINGS.get("SNDTIMEO", 60000))
+        r.setsockopt(zmq.RCVTIMEO, settings.ZMQ_SETTINGS.get("RCVTIMEO", 60000))
+        r.connect(sync_address)
+        r.send(b"Sync message")
+        sys.stdout.write("Send request to model\n")
+        r.recv()
+        sys.stdout.write("Model is ready\n")
+        sys.stdout.flush()
+        r.close()
+
+    async def receive(self) -> ResponseBatch:
+
+        data = self.zmq_socket.recv_pyobj()
+        try:
+            minimal_batch = data.get("batch_object")
+            outputs = data.get("outputs")
+            pictures = data.get("pictures")
+
+            if minimal_batch is None:
+                sys.stdout.write("Minimal batch object is None")
+                return None
+
+            # Check that pictures is not None
+            if pictures is not None and pictures.size != 0:
+                response_outputs = [{"outputs": outputs, "pictures": pictures}]
+            else:
+                response_outputs = [{"outputs": outputs}]
+
+            response = ResponseBatch.from_minimal_batch_object(
+                batch=minimal_batch,
+                outputs=response_outputs,
+                source_id=minimal_batch.source_id,
+                done_at=minimal_batch.done_at,
+            )
+            return response
+        except KeyError:
+            logger.error("No such key in dictionary")
 
     def close(self):
         self.zmq_socket.close()
--- task_manager/src/utils/data_transfers/sender.py
+++ task_manager/src/utils/data_transfers/sender.py
@@ -8,47 +8,36 @@ __name__ = "a.chertkov@eora.ru"
 import zmq
 import sys
 
-import src.data_models as dm
-
 
 class Sender:
     """Opens zmq PUSH socket and sends RequestBatchObject"""
 
     def __init__(self, open_address, sync_address, settings):
         self.zmq_context = zmq.Context()
-        self.zmq_socket = self.smq_context.socket(zmq.PUSH)
-        self.zmq_socket.setsockopt(zmq.SNDHWM, settings.get("SNDHWM", 10))
-        self.zmq_socket.setsockopt(zmq.SNDTIMEO, settings.get("SNDTIMEO", 60000))
+        self.zmq_socket = self.zmq_context.socket(zmq.PUSH)
+        self.zmq_socket.setsockopt(zmq.SNDHWM, settings.ZMQ_SETTINGS.get("SNDHWM", 10))
+        self.zmq_socket.setsockopt(
+            zmq.SNDTIMEO, settings.ZMQ_SETTINGS.get("SNDTIMEO", 60000)
+        )
         self.zmq_socket.connect(open_address)
-        # TODO Add sync
+        self.sync(sync_address, settings)
 
-    async def sync(self, sync_address, settings):
-        # TODO: how to make it async
-
+    def sync(self, sync_address, settings):
         ctx = zmq.Context.instance()
         r = ctx.socket(zmq.REQ)
-        r.setsockopt(zmq.SNDTIMEO, settings.get("SNDTIMEO", 60000))
-        r.setsockopt(zmq.RCVTIMEO, settings.get("RCVTIMEO", 60000))
+        r.setsockopt(zmq.SNDTIMEO, settings.ZMQ_SETTINGS.get("SNDTIMEO", 60000))
+        r.setsockopt(zmq.RCVTIMEO, settings.ZMQ_SETTINGS.get("RCVTIMEO", 60000))
         r.connect(sync_address)
-        sys.stdout.write("Connect to model\n")
-        await r.send()
+        r.send(b"Sync message")
         sys.stdout.write("Send request to model\n")
         r.recv()
         sys.stdout.write("Model is ready\n")
         sys.stdout.flush()
         r.close()
 
-    async def send(self, request_obj: dm.RequestObject):
-        uid_request_obj = request_obj.uid
-        inputs_request_obj = request_obj.inputs
-        parameters_request_obj = request_obj.parameters
-
-        data = {
-            "uid": uid_request_obj,
-            "inputs": inputs_request_obj,
-            "parameters": parameters_request_obj,
-        }
-        await self.zmq_socket.send_pyobj(data)
+    async def send(self, minimal_batch_obj):
+        data = {"batch_object": minimal_batch_obj}
+        self.zmq_socket.send_pyobj(data)
 
     def close(self):
         self.zmq_socket.close()

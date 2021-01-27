dd5259c21848109885aedfdf17aa336101f78114
Andrey Chertkov <andreychertckov@gmail.com>
2021-01-27 11:01:11 +0300

    Add connectionchecker

--- task_manager/src/health_checker/connection_stable_checker.py
+++ task_manager/src/health_checker/connection_stable_checker.py
@@ -5,6 +5,8 @@ Connection stable checker
 __author__ = "Andrey Chertkov"
 __email__ = "a.chertkov@eora.ru"
 
+import time
+
 import src.data_models as dm
 from .checker import BaseHealthChecker, Status
 
@@ -14,4 +16,16 @@ class ConnectionChecker(BaseHealthChecker):
         if not model_instance.lock:
             return Status(model_instance=model_instance, is_running=True, reason=None)
 
-        return Status(model_instance=model_instance, is_running=True, reason=None)
+        last_sent_batch = model_instance.sender.get_time_of_last_sent_batch()
+
+        last_received_batch = model_instance.receiver.get_time_of_last_received_batch()
+
+        cur = time.time()
+        if cur - last_sent_batch < 10 or cur - last_received_batch < 10:
+            return Status(model_instance=model_instance, is_running=True, reason=None)
+
+        return Status(
+            model_instance=model_instance,
+            is_running=False,
+            reason="Nothing was sent or received in 10 seconds",
+        )
--- task_manager/src/utils/data_transfers/receiver.py
+++ task_manager/src/utils/data_transfers/receiver.py
@@ -5,15 +5,18 @@ This module is responsible for receiving data from model instance
 __author__ = "Andrey Chertkov"
 __email__ = "a.chertkov@eora.ru"
 
-import zmq.asyncio  # type: ignore
+import time
 from typing import AsyncIterator
 
+import zmq.asyncio  # type: ignore
 from loguru import logger
+
 from shared_modules.data_objects import ResponseBatch, ZMQConfig
 
 
 class BaseReceiver:
     def __init__(self):
+        self.last_received_batch = time.time()
         pass
 
     def sync(self, sync_address, config):
@@ -25,6 +28,9 @@ class BaseReceiver:
     def close(self):
         pass
 
+    def get_time_of_last_received_batch(self):
+        return self.last_received_batch
+
 
 class Receiver(BaseReceiver):
     def __init__(self, open_address: str, sync_address: str, config: ZMQConfig):
@@ -34,6 +40,7 @@ class Receiver(BaseReceiver):
         self.zmq_socket.setsockopt(zmq.RCVTIMEO, config.rcvtimeo)
         self.zmq_socket.connect(open_address)
         self.sync(sync_address, config)
+        self.last_received_batch = time.time()
 
     def sync(self, sync_address: str, config: ZMQConfig):
         self.zmq_context = zmq.asyncio.Context()
@@ -52,7 +59,12 @@ class Receiver(BaseReceiver):
             if response_batch is None:
                 logger.warning("Response batch object is None")
 
+            self.last_received_batch = time.time()
+
             yield response_batch
 
     def close(self):
         self.zmq_socket.close()
+
+    def get_time_of_last_received_batch(self):
+        return self.last_received_batch

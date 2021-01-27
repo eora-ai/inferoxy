2f73a4155a31257442a83601d91242a0420b2c4c
Andrey Chertkov <andreychertckov@gmail.com>
2021-01-27 08:54:14 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 collect_ignore = [
     "task_manager/tests/docker_integration_tests/",
     "task_manager/tests/test_sender.py",
+    "task_manager/tests/test_health_checkers/test_container_running_with_docker_cloud_client.py",
 ]

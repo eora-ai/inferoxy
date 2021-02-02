27dc7d6ed820f4e6d697cbb8fdc0f59af240aa84
gafmn <madina.gafarova@yandex.ru>
2021-02-02 10:37:33 +0300

    Add test video without sound

--- task_manager/test_client/main.py
+++ task_manager/test_client/main.py
@@ -7,13 +7,14 @@ __email__ = "m.gafarova@eora.ru"
 
 
 import sys
+import os
 import yaml
 import zmq
 import pathlib
+import numpy as np
 
 from moviepy.editor import VideoFileClip
 from PIL import Image  # type: ignore
-from numpy import asarray  # type: ignore
 from loguru import logger
 
 sys.path.append("..")
@@ -21,6 +22,7 @@ sys.path.append("..")
 import src.data_models as dm
 
 from src.data_models import MinimalBatchObject, ModelObject
+from shared_modules.utils import uuid4_string_generator
 
 
 stub_model = model = ModelObject(
@@ -31,9 +33,52 @@ stub_model = model = ModelObject(
 )
 
 
+def extract_frames_at_times(movie, times, imgdir):
+    clip = VideoFileClip(movie)
+    for t in times:
+        imgpath = os.path.join(imgdir, "{}.png".format(t))
+        clip.save_frame(imgpath, t)
+        #  load the image
+        image = Image.open(imgpath)
+
+        # convert image to numpy array
+        data = np.asarray(image)
+        yield data
+
+
 def batch_video():
-    video = VideoFileClip("without_sound_cutted.mp4")
-    print([frame for frame in video.iter_frames()])
+    cur_path = pathlib.Path(__file__)
+    config_path = cur_path.parent.parent / "config.yaml"
+
+    with open(config_path) as config_file:
+        config_dict = yaml.full_load(config_file)
+        config = dm.Config.from_dict(config_dict)
+
+    ctx = zmq.Context()
+    sock_sender = ctx.socket(zmq.PUB)
+    sock_sender.connect(config.zmq_input_address)
+    sock_receiver = ctx.socket(zmq.SUB)
+    sock_receiver.bind(config.zmq_output_address)
+    sock_receiver.subscribe(b"")
+
+    movie = VideoFileClip("without_sound_cutted.mp4")
+    uid_generator = uuid4_string_generator()
+    for frame in movie.iter_frames():
+        request_info = dm.RequestInfo(
+            input=frame,
+            parameters={},
+        )
+        batch = MinimalBatchObject(
+            uid=next(uid_generator),
+            requests_info=[request_info],
+            model=stub_model,
+        )
+        print(batch)
+        sock_sender.send_pyobj(batch)
+    logger.info("Start listening")
+    while True:
+        result = sock_receiver.recv_pyobj()
+        logger.info(f"Result batch {result}")
 
 
 def batch_pictures():
@@ -55,7 +100,7 @@ def batch_pictures():
     image = Image.open("test.jpg")
 
     # convert image to numpy array
-    data = asarray(image)
+    data = np.asarray(image)
 
     request_info = dm.RequestInfo(
         input=data,

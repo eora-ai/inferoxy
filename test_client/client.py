6e09492a82c82c4c891123b5ce17d9cb6d417dbe
Andrey Chertkov <andreychertckov@gmail.com>
2021-03-05 12:52:48 +0300

    Rewrite with ROUTER/DEALER

--- test_client/client.py
+++ test_client/client.py
@@ -2,6 +2,8 @@
 Test clients for listener
 """
 
+import uuid
+
 from loguru import logger
 import zmq
 import numpy as np
@@ -14,7 +16,9 @@ def main():
     input_socket = context.socket(zmq.PUSH)
     input_socket.connect("tcp://localhost:7787")
     logger.info("Connected to receiver")
-    output_socket = context.socket(zmq.SUB)
+    output_socket = context.socket(zmq.DEALER)
+    uid = f"test_{uuid.uuid4()}.jpg"
+    output_socket.setsockopt(zmq.IDENTITY, uid.encode("utf-8"))
     output_socket.connect("tcp://localhost:7788")
 
     stateless = input("y/n: ") == "y"
@@ -23,25 +27,25 @@ def main():
     image = Image.open("test.jpg")
     image_array = np.asarray(image)
 
-    output_socket.subscribe(b"test.jpg")
     logger.info("Connected to sender")
 
     for i in range(number_of_request):
 
         input_socket.send_pyobj(
             {
-                "source_id": "test.jpg",
+                "source_id": uid,
                 "input": image_array,
-                "parameters": {"stateless": stateless},
+                "parameters": {"stateless": stateless, "index": i},
                 "model": "stub",
             }
         )
         logger.info(f"Sent {i}")
 
     for i in range(number_of_request):
-        topic = output_socket.recv_string()
         result = output_socket.recv_pyobj()
-        logger.info(f"{topic} -> {result['uid']} -> {i}")
+        logger.info(
+            f"{i} -> {result['uid']} -> {result['response_info']['parameters']['index']}"
+        )
 
 
 if __name__ == "__main__":
